8e90b40cc101a7d09fd1199cc828427bdf4bfef8
ColeeneP <coleene.poudevigne@live.fr>
2024-09-12 19:42:30 +0200

    add interfaces

---
+++ backend/src/Interfaces/article.interface.ts
@@ -0,0 +1,8 @@
+export interface Article {
+    id: number;
+    id_user: number;
+    date: Date;
+    title: string;
+    content: string;
+    id_credits: number;
+}
---
+++ backend/src/Interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface Article {
+    id: number;
+    pseudo: string;
+    email: string;
+    password: string;
+    phone: string;
+}
